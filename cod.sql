216c0c38d7d5cf1ad2c4426fda422fa4eac7d87a
Lucas Queiroz <161960484+LcasQueirxz@users.noreply.github.com>
2024-09-21 11:23:00 -0300

    Add files via upload

---
+++ cod.sql
@@ -0,0 +1,23 @@
+insert into Client(
+id,
+nome,
+idade ,
+email,
+altura,
+vip 
+)
+Values (
+1,
+  'Claudio',
+  39,
+  'cla.almeida@gmail.com',
+  1.78,
+  true
+),
+  (2,
+  'lucas',
+  19,
+  'lucas.almeida@gmail.com',
+  1.77,
+  true
+  );
